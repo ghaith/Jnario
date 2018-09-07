39244baadbdcbca26508020c97ddee1935e820e2
Sebastian Pötzsch <sebastian.poetzsch@fv.nrw.de>
2018-09-07 08:17:30 +0200

    Fix Folding for Specs
    Change-Id: I3465812d987b27054f27c9b72672e00f36ddf7d2

--- plugins/org.jnario.spec.ui/src/org/jnario/spec/ui/editor/SpecFoldingRegionProvider.java
+++ plugins/org.jnario.spec.ui/src/org/jnario/spec/ui/editor/SpecFoldingRegionProvider.java
@@ -10,23 +10,46 @@ package org.jnario.spec.ui.editor;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.ui.editor.folding.DefaultFoldingRegionProvider;
+import org.eclipse.xtext.ui.editor.folding.IFoldingRegionAcceptor;
+import org.eclipse.xtext.util.ITextRegion;
+import org.eclipse.xtext.xtype.XImportSection;
+import org.jnario.JnarioPackage;
+import org.jnario.spec.spec.SpecFile;
 import org.jnario.spec.spec.SpecPackage;
 
 /**
  * @author Sebastian Benz - Initial contribution and API
  */
-@SuppressWarnings("restriction")
 public class SpecFoldingRegionProvider extends DefaultFoldingRegionProvider {
 
 	@Override
 	protected boolean isHandled(EObject object) {
 		EClass clazz = object.eClass();
-		return clazz == SpecPackage.Literals.EXAMPLE_GROUP || clazz == SpecPackage.Literals.EXAMPLE || super.isHandled(object);
+		return clazz == SpecPackage.Literals.SPEC_FILE 
+				|| clazz == SpecPackage.Literals.EXAMPLE_GROUP
+				|| clazz == SpecPackage.Literals.EXAMPLE
+				|| clazz == SpecPackage.Literals.AFTER
+				|| clazz == SpecPackage.Literals.BEFORE
+				|| clazz == SpecPackage.Literals.TEST_FUNCTION
+				|| clazz == JnarioPackage.Literals.JNARIO_FUNCTION
+		;
 	}
 	
 	@Override
 	protected boolean shouldProcessContent(EObject object) {
-		EClass clazz = object.eClass();
-		return clazz == SpecPackage.Literals.EXAMPLE_GROUP || clazz == SpecPackage.Literals.EXAMPLE || super.isHandled(object);
+		return true;
+	}
+	
+	@Override
+	protected void computeObjectFolding(EObject eObject, IFoldingRegionAcceptor<ITextRegion> foldingRegionAcceptor) {
+		EClass clazz = eObject.eClass();
+		if(clazz == SpecPackage.Literals.SPEC_FILE) {
+            XImportSection importSection = ((SpecFile)eObject).getImportSection();
+            if (importSection != null) {
+                super.computeObjectFolding(importSection, foldingRegionAcceptor, true);
+                return;
+            }
+		}
+        super.computeObjectFolding(eObject, foldingRegionAcceptor);
 	}
 }
